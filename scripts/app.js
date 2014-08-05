20b4cda8c5f1e3e6ce8ab4f8000b8cf8f9bc3742
eat-sleep-code/ <info@eat-sleep-code.com>
2014-08-05 13:34:46 -0700

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+var app = angular.module('eatsleepcode', ['ngRoute', 'ngSanitize']); 
+
+/* Routing */
+app.config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {    
+		$routeProvider.      
+			/* Root */
+			when('/', {templateUrl: 'views/send.html', controller: 'PostController'}).
+			when('/send', {templateUrl: 'views/send.html', controller: 'PostController'}).
+			when('/post', {templateUrl: 'views/send.html', controller: 'PostController'}).
+			when('/post/:postID', {templateUrl: 'views/blog.html', controller: 'PostController'}).
+			when('/contact', {templateUrl: 'views/contact.html', controller: 'DefaultController'}).
+			when('/privacy', {templateUrl: 'views/privacy.html', controller: 'DefaultController'}).
+			when('/terms', {templateUrl: 'views/terms.html', controller: 'DefaultController'}).
+			when('/404', {templateUrl: 'views/404.html', controller: 'DefaultController'}).
+			otherwise({
+				redirectTo: '/404'
+			});
+			$locationProvider.html5Mode(false);
+			$locationProvider.hashPrefix("!");
+}]);  
+
+
+/* Controllers */
+app.controller('DefaultController', function($scope) {});
+
+app.controller('PostController', function($scope, $routeParams, $http) {});
+
+
+/* Filters */
+app.filter('slice', function() {
+  return function(arr, start, end) {
+  	arr = arr || [];
+  	/* console.log('Slice Arr: ' + arr);
+  	console.log('Slice Start: ' + start);
+  	console.log('Slice End: ' + end); */
+    return arr.slice(start, end);
+  };
+});
+
+
+
