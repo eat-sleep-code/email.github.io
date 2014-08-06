7781a39debdb5a8b36503015476eaec06bd6090a
eat-sleep-code/ <info@eat-sleep-code.com>
2014-08-06 14:23:16 -0700

    Update mail.js

--- scripts/mail.js
+++ scripts/mail.js
@@ -35,7 +35,7 @@ $('#ContactMessageSendButton').click(function(e) {
 					var senderName = 'emailapp.eat-sleep-code.com';
 					var recipient = $.trim($('#ToEmailAddress').val());
 					var subject = $.trim($('#Subject').val());
-					var message = $.trim($('#Message').val()).replace(/\n/g, '<br />') + '<br />');
+					var message = $.trim($('#Message').val()).replace(/\n/g, '<br />') + '<br />';
 					var messageHeader = 'This message was sent to you anonymously.   If you would like to send a reply, <a href="http://emailapp.eat-sleep-code.com/reply/'+postID+'" target="_blank" title="Click here to reply to this message">click here</a>.';
 					var messageFooter = 'If you would like to block all emailapp.eat-sleep-code.com users from sending you a message, <a href="http://emailapp.eat-sleep-code.com/block/'+postID+'" target="_blank" title="Click here to block users from sending you a message.">click here</a>.';
 				
