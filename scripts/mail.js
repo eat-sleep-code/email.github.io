a4e1be68050d9abce38e58671b45bd4370e57371
eat-sleep-code/ <info@eat-sleep-code.com>
2014-08-06 20:50:22 -0700

    Update mail.js

--- scripts/mail.js
+++ scripts/mail.js
@@ -26,27 +26,51 @@ $('#ContactMessageSendButton').click(function(e) {
 		var form = $('#ContactMessageForm');
 		form.validate();
 		if (form.valid() === true) {
-			/* Get IP Address */
-			//$.get("http://ipinfo.io", function(response) {
-				/* Send mail only if there is a message (basically prevent empty submissions if validation goes haywire)... */
-				if ($.trim($('#Message').val()).length >= 1) {
-					var postID = $.trim($('#PostIDHidden').val());
-					var sender = 'info@eat-sleep-code.com';
-					var senderName = 'emailapp.eat-sleep-code.com';
-					var recipient = $.trim($('#ToEmailAddress').val());
-					var subject = $.trim($('#Subject').val());
-					var message = $.trim($('#Message').val()).replace(/\n/g, '<br />') + '<br />';
-					var messageHeader = 'This message was sent to you anonymously.   If you would like to send a reply, <a href="http://emailapp.eat-sleep-code.com/reply/'+postID+'" target="_blank" title="Click here to reply to this message">click here</a>.';
-					var messageFooter = 'If you would like to block all emailapp.eat-sleep-code.com users from sending you a message, <a href="http://emailapp.eat-sleep-code.com/block/'+postID+'" target="_blank" title="Click here to block users from sending you a message.">click here</a>.';
+			if ($.trim($('#Message').val()).length >= 1) {
+			/* Send mail only if there is a message (basically prevent empty submissions if validation goes haywire)... */
+				var serviceName = 'emailapp.eat-sleep-code.com';
+				var serviceEmail = 'info@eat-sleep-code.com';
+				var postID = $.trim($('#PostIDHidden').val());
+				var sender = $.trim($('#FromEmailAddress').val());;
+				var recipient = $.trim($('#ToEmailAddress').val());
+				var subject = $.trim($('#Subject').val());
+				var message = $.trim($('#Message').val()).replace(/\n/g, '<br />') + '<br />';
+				var messageHeader = 'This message was sent to you anonymously.   If you would like to send a reply, <a href="http://emailapp.eat-sleep-code.com/reply/'+postID+'" target="_blank" title="Click here to reply to this message">click here</a>.';
+				var messageFooter = 'If you would like to block all emailapp.eat-sleep-code.com users from sending you a message, <a href="http://emailapp.eat-sleep-code.com/block/'+postID+'" target="_blank" title="Click here to block users from sending you a message.">click here</a>.';
+			
+				// Check To Make Sure Recipient Hasn't Blocked Service
+				var recipientCleared = true;
 				
-					//messageFooter += '<hr />' + response.ip + '<br />' + response.city + ', ' + response.region + ' ' + response.postal + ', ' + response.country;
-					sendMail(sender, senderName, recipient, subject, messageHeader + '<hr />' + message + '<hr />' + messageFooter);
-					
+				if (recipientCleared == true) {
+					// Save To Database
+					$.ajax({
+						url: "https://docs.google.com/forms/d/1Q79Y1ETyJU0ffMreELbiIY5qf2OjGdeyNrzNMXip2Lc/formResponse",
+						data: { 
+							"entry_1063432399": postID, 
+							"entry_274482486": sender, 
+							"entry_692194785": recipient, 
+							"entry_848437565": subject, 
+							"entry_1901043579": message
+						},
+						type: "POST",
+						dataType: "xml",
+						statusCode: {
+							0: function () {
+								// Send Mail 
+						    		sendMail(serviceEmail, serviceName, recipient, subject, messageHeader + '<hr />' + message + '<hr />' + messageFooter);
+							},
+							200: function () {
+								// Send Mail 
+						    		sendMail(serviceEmail, serviceName, recipient, subject, messageHeader + '<hr />' + message + '<hr />' + messageFooter);
+							}
+						}
+					});
+	
 					$('#ContactMessageSentAlert').show();
 					$('#ContactMessageForm').hide();
 					$.cookie('ClearedCaptcha', 'true', { expires: 7, path: '/' });
-				}	
-			//}, "jsonp");
+				}
+			}	
 			$(window).scrollTop(0);
 		}
 	});
