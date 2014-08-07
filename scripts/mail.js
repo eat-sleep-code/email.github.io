102d7d70ab75632f67013be805b0e29d90909576
eat-sleep-code/ <info@eat-sleep-code.com>
2014-08-07 10:28:07 -0700

    Update mail.js

--- scripts/mail.js
+++ scripts/mail.js
@@ -29,14 +29,15 @@ $('#ContactMessageSendButton').click(function(e) {
 			if ($.trim($('#Message').val()).length >= 1) {
 			/* Send mail only if there is a message (basically prevent empty submissions if validation goes haywire)... */
 				var serviceName = 'emailapp.eat-sleep-code.com';
+				var serviceRootUrl = 'http://emailapp.eat-sleep-code.com';
 				var serviceEmail = 'info@eat-sleep-code.com';
 				var postID = $.trim($('#PostIDHidden').val());
 				var sender = $.trim($('#FromEmailAddress').val());;
 				var recipient = $.trim($('#ToEmailAddress').val());
 				var subject = $.trim($('#Subject').val());
 				var message = $.trim($('#Message').val()).replace(/\n/g, '<br />') + '<br />';
-				var messageHeader = 'This message was sent to you anonymously.   If you would like to send a reply, <a href="http://emailapp.eat-sleep-code.com/reply/'+postID+'" target="_blank" title="Click here to reply to this message">click here</a>.';
-				var messageFooter = 'If you would like to block all emailapp.eat-sleep-code.com users from sending you a message, <a href="http://emailapp.eat-sleep-code.com/block/'+postID+'" target="_blank" title="Click here to block users from sending you a message.">click here</a>.';
+				var messageHeader = 'This message was sent to you anonymously.   If you would like to send a reply, <a href="'+serviceRootUrl+'/reply/'+postID+'" target="_blank" title="Click here to reply to this message">click here</a>.';
+				var messageFooter = 'If you would like to block all ' + serviceName + ' users from sending you a message, <a href="'+serviceRootUrl+'/unsubscribe/'+postID+'" target="_blank" title="Click here to block users from sending you future messages.">click here</a>.';
 			
 				// Check To Make Sure Recipient Hasn't Blocked Service
 				var recipientCleared = true;
