24a695b28549110b3c1b14b3e40b706b1e9b8ba6
eat-sleep-code/ <info@eat-sleep-code.com>
2014-08-07 11:21:28 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
-var messageDatasource='https://docs.google.com/spreadsheets/d/1A489xTOwSGT3dYNiqhyzB6JvHk8rMUjhAjOQ_bSCDWQ#gid=0';
-var unsubscribeDatasource='https://docs.google.com/spreadsheets/d/1wUilI5prEx6ocuCh91Za_uc_F2cOZx3zzamMO46hSx8#gid=0';
+// Global Variables
+var serviceName = 'emailapp.eat-sleep-code.com';
+var serviceRootUrl = 'http://emailapp.eat-sleep-code.com';
+var serviceEmail = 'info@eat-sleep-code.com';
+				
+// Data Connections
+var messageDatasourceWrite = 'https://docs.google.com/forms/d/1Q79Y1ETyJU0ffMreELbiIY5qf2OjGdeyNrzNMXip2Lc/formResponse';
+var messageDatasourceRead = 'https://docs.google.com/spreadsheets/d/1A489xTOwSGT3dYNiqhyzB6JvHk8rMUjhAjOQ_bSCDWQ#gid=0';
+var unsubscribeDatasourceWrite = 'https://docs.google.com/forms/d/1JkF4sP5G4HVJAAlP4NKPVlnFaJQtAXm6yL2I8dLPwtE/formResponse';
+var unsubscribeDatasourceRead = 'https://docs.google.com/spreadsheets/d/1wUilI5prEx6ocuCh91Za_uc_F2cOZx3zzamMO46hSx8#gid=0';
 
 var app = angular.module('eatsleepcode', ['ngRoute', 'ngSanitize']); 
 
@@ -8,9 +16,13 @@ app.config(['$routeProvider', '$locationProvider',
 	function($routeProvider, $locationProvider) {    
 		$routeProvider.      
 			/* Root */
-			when('/', {templateUrl: 'views/send.html', controller: 'PostController'}).
-			when('/send', {templateUrl: 'views/send.html', controller: 'PostController'}).
-			when('/send/:postID', {templateUrl: 'views/send.html', controller: 'PostController'}).
+			when('/', {templateUrl: 'views/message.html', controller: 'MessageController'}).
+			when('/message', {templateUrl: 'views/message.html', controller: 'MessageController'}).
+			when('/send', {templateUrl: 'views/message.html', controller: 'MessageController'}).
+			when('/message/:messageID', {templateUrl: 'views/message.html', controller: 'MessageController'}).
+			when('/reply/:messageID', {templateUrl: 'views/message.html', controller: 'MessageController'}).
+			when('/unsubscribe', {templateUrl: 'views/unsubscribe.html', controller: 'UnsubscribeController'}).
+			when('/unsubscribe/:emailAddress', {templateUrl: 'views/unsubscribe.html', controller: 'UnsubscribeController'}).
 			when('/contact', {templateUrl: 'views/contact.html', controller: 'DefaultController'}).
 			when('/privacy', {templateUrl: 'views/privacy.html', controller: 'DefaultController'}).
 			when('/resources', {templateUrl: 'views/resources.html', controller: 'DefaultController'}).
@@ -27,12 +39,17 @@ app.config(['$routeProvider', '$locationProvider',
 /* Controllers */
 app.controller('DefaultController', function($scope) {});
 
-app.controller('PostController', function($scope, $routeParams, $http) {
-	$scope.post = $routeParams.postID;
-	$scope.data = "";
-	$scope.status = "";
+app.controller('MessageController', function($scope, $routeParams, $http) {
+	$scope.message = $routeParams.messageID;
+	$scope.data = '';
+	$scope.status = '';
 });
 
+app.controller('UnsubscribeController', function($scope, $routeParams, $http) {
+	$scope.email = $routeParams.emailAddress;
+	$scope.data = '';
+	$scope.status = '';
+});
 
 /* Filters */
 app.filter('slice', function() {
